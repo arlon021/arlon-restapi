12e5ada7d342bd1cd66e563e660b24446e050181
Arlon <arlon20092009@gmail.com>
2024-03-12 17:32:16 -0300

    subindo codigo inteiro

---
+++ src/main/java/br/com/arlon/ArlonMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.arlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArlonMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArlonMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arlon/ArlonMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arlon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArlonMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
